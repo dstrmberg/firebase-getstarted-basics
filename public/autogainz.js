b5849bf99fa28c3acd99fcb90f1b53c5a9549a37
Dino Wiid Strömberg <d@d3s.se>
2022-05-15 17:17:09 +0200

    First commit

---
+++ public/autogainz.js
@@ -0,0 +1,79 @@
+console.log(firebase)
+
+const signInButton = document.getElementById('signInButton');
+const signOutButton = document.getElementById('signOutButton');
+
+// Set up authentication
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+// Event handlers
+signInButton.onclick = () => auth.signInWithPopup(provider);
+signOutButton.onclick = () => auth.signOut();
+
+const signedIn = document.getElementById('signedIn');
+const signedOut = document.getElementById('signedOut');
+const userDetails = document.getElementById('userDetails');
+
+auth.onAuthStateChanged(user => {
+    if (user) {
+        // signed in
+        signedIn.hidden = false;
+        signedOut.hidden = true;
+        userDetails.innerHTML = `<h3>Hello ${user.displayName}!</h3> <p>User ID: ${user.uid}</p>`;
+    } else {
+        // not signed in
+        signedIn.hidden = true;
+        signedOut.hidden = false;
+        userDetails.innerHTML = '';
+    }
+});
+
+const excerciseName = document.getElementById('excerciseName');
+const createExcercise = document.getElementById('createExcercise');
+const exercisesList = document.getElementById('exercisesList');
+
+const db = firebase.firestore();
+let exercisesReference;
+let unsubscribe;
+
+auth.onAuthStateChanged(user => {
+
+    if (user) {
+
+        // Database Reference
+        exercisesReference = db.collection('exercises')
+
+        createExcercise.onclick = () => {
+
+        const { serverTimestamp } = firebase.firestore.FieldValue;
+
+        exercisesReference.add({
+            uid: user.uid,
+            name: excerciseName.value,
+            createdAt: serverTimestamp()
+        });
+
+        }
+
+        // Query
+        unsubscribe = exercisesReference.where('uid', '==', user.uid)
+            .onSnapshot(querySnapshot => {
+                
+                // Map results to an array of li elements
+
+                const items = querySnapshot.docs.map(doc => {
+
+                    return `<li>${doc.data().name}</li>`
+
+                });
+
+                exercisesList.innerHTML = items.join('');
+
+            });
+
+    } else {
+        // Unsubscribe when the user signs out
+        unsubscribe && unsubscribe();
+    }
+});
